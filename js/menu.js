d6fd974a318d960af985a881c00aba365d988540
Pavlo Kravchuk <kravchuk.pavlo@ukr.net>
2021-06-20 14:05:49 +0200

     Розмітка модального меню

--- js/menu.js
+++ js/menu.js
@@ -1,14 +1,14 @@
+
 (() => {
-    const menuBtnRef = document.querySelector("[data-menu-button]");
-    const mobileMenu = document.querySelector("[data-menu]");
+  const menuBtnRef = document.querySelector("[data-menu-button]");
+  const mobileMenuRef = document.querySelector("[data-menu]");
+  const mobileBtnClose = document.querySelector("[data-menu-close]");
 
-    menuBtnRef.addEventListener("click", () => {
-        const expanded =
-            menuBtnRef.getAttribute("aria-expanded") === "true" || false;
-        
-        menuBtnRef.classList.toggle("is-open");
-        menuBtnRef.setAttribute("aria-expanded", !expanded);
+  menuBtnRef.addEventListener("click", () => {
+    mobileMenuRef.classList.toggle("is-open");
+  });
 
-        mobileMenuRef.classList.toggle("is-open");
-    });
+  mobileBtnClose.addEventListener("click", () => {
+    mobileMenuRef.classList.toggle("is-open");
+  });
 })();
